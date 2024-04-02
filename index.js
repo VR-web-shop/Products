c029810afdbc97c78332078b72b5c8faed36dba1
Nicolai <cwX3MfXK9YrCCc8eZk@gmail.com>
2024-04-02 04:35:24 +0200

    update

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ import { connect } from './src/config/BrokerConfig.js';
 import express from 'express';
 import cors from 'cors';
 
-import ProductController from './src/controllers/api/v1/ProductController.js';
-import ProductEntityController from './src/controllers/api/v1/ProductEntityController.js';
-import ProductEntityStateController from './src/controllers/api/v1/ProductEntityStateController.js';
+import Controller from './src/controllers/api/v1/Controller.js';
 import SwaggerController from './src/controllers/SwaggerController.js';
 
 (async () => {
@@ -18,9 +16,9 @@ import SwaggerController from './src/controllers/SwaggerController.js';
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
     app.use(SwaggerController);
-    app.use(ProductController);
-    app.use(ProductEntityController);
-    app.use(ProductEntityStateController);
+    Object.values(Controller).forEach(controller => {
+        app.use(controller.router)
+    });
 
     app.listen(process.env.SERVER_PORT, () => {
         console.log(`Server is running on port ${process.env.SERVER_PORT}`);
